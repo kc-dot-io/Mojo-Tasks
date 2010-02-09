5904bd75271e9f2918588dbb1b2e364c69406092
slajax <mail@slajax.com>
2010-02-09 10:36:14 -0800

    updates for build script

--- src/MojoBuild.class.php
+++ src/MojoBuild.class.php
@@ -35,52 +35,39 @@ class MojoBuild extends Mojo
     $dependencies[] = $js_dir.'extlib/mootools-core.js';
     $dependencies[] = $js_dir.'extlib/mootools-more.js';
     $dependencies[] = $js_dir.'extlib/s_code-20.3.js';
-
+    $dependencies[] = $js_dir.MojoConfig::get('mojo_app_name').'/services/Locator.js';
+    
     $sitemap = MojoConfig::get('mojo_js_dir') . $app . 'SiteMap.js';
     $src = file_get_contents($sitemap);
-   
-    
+       
     jsc::compile($src);       
     
     foreach($bc['.'] as $k => $v){
      
       
-      if(isset($v[7])){ //sitemap entries
+    if(isset($v[7])){ //sitemap entries
+    
+     if(isset($v[7][2][1])){ //controllers
+      $controller = str_replace("'","",$v[7][2][1]);
+      $controller = $js_dir.join('/',explode('.',$controller)).'.js';
+      $controllers[] = $controller;      
+     
+      if(!array_search($controller,$dependencies))
+        $dependencies[] = $controller;
+      }
       
-       if(isset($v[7][2][1])){ //controllers
-        $controller = str_replace("'","",$v[7][2][1]);
-        $controller = $js_dir.join('/',explode('.',$controller)).'.js';
-        $controllers[] = $controller;      
-       
-        if(!array_search($controller,$dependencies))
-          $dependencies[] = $controller;
+      if(isset($v[7][6][0][1]) && isset($v[7][6][2][1])){ //rules
+        if($v[7][6][0][1] == 'formrules'){
+          $rule = str_replace("'","",$v[7][6][2][1]);
+          $rule = $js_dir.join('/',explode('.',$rule)).'.js';
+          $rules[] = $rule;
+          if(!array_search($rule,$dependencies))
+            $dependencies[] = $rule;              
         }
-        
-        if(isset($v[7][6][0][1]) && isset($v[7][6][2][1])){ //rules
-          if($v[7][6][0][1] == 'formrules'){
-            $rule = str_replace("'","",$v[7][6][2][1]);
-            $rule = $js_dir.join('/',explode('.',$rule)).'.js';
-            $rules[] = $rule;
-            if(!array_search($rule,$dependencies))
-              $dependencies[] = $rule;              
-          }
-        }
-       }
+      }
+     }
     }
     
-    foreach($controllers as $controller){ //commands
-            
-      $c = file_get_contents($controller);
-      preg_match_all("/addCommand[^,]*[^\"']*('|\")([^'\"]*)('|\")/",$c,$matches);
-      
-      foreach($matches[2] as $commands => $command){        
-        $command = $js_dir.join('/',explode('.',$command)).'.js';
-        
-        if(!empty($command) && !array_search($command,$dependencies))
-          $dependencies[] = $command;          
-      }
-    }   
-  
     $i18n = MojoFile::getAll(MojoConfig::get('mojo_js_dir').'_i18n/');
     
     foreach($rules as $rule){
@@ -96,22 +83,36 @@ class MojoBuild extends Mojo
         }
       }
     }    
-     
     
+    foreach($controllers as $controller){ //commands
+            
+      $c = file_get_contents($controller);
+      preg_match_all("/addCommand[^,]*[^\"']*('|\")([^'\"]*)('|\")/",$c,$matches);
+      
+      foreach($matches[2] as $commands => $command){        
+        $command = $js_dir.join('/',explode('.',$command)).'.js';
+        
+        if(!empty($command) && !array_search($command,$dependencies))
+          $dependencies[] = $command;          
+      }
+    }   
+
     $config = (!empty($app))
       ?MojoConfig::get('mojo_app_name').'.'.strtolower($app).'.config.js'
-      :MojoConfig::get('mojo_app_name').'.config.js';
+      :MojoConfig::get('mojo_app_name').'.config.js';    
     
-    $dependencies[] = $js_dir.MojoConfig::get('mojo_app_name').'/services/Locator.js';
-    $dependencies[] = $js_dir.$config;    
+    $dependencies[] = $js_dir.$config;       
     
-    print_r($dependencies); exit;
+    #print_r($dependencies); exit;
     
     $js = "";
     foreach($dependencies as $dependency){
      $js .= file_get_contents( $dependency );
     }
     
+    $js = preg_replace( '/dojo\.require\("[^\)]+"\);/i','',$js);    
+    $js = preg_replace( "/dojo\.require\('[^\)]+'\);/i",'',$js);
+
     if(MojoFile::write($js_dir.'application.uncompressed.js',$js))
       Mojo::prompt('application.uncompressed.js written to '.$js_dir.'application.uncompressed.js');
     
