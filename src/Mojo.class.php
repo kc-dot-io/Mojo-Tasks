05be0ca36fa2788eb781bedcb3d4084e2578c3c9
slajax <mail@slajax.com>
2010-02-10 17:17:05 -0500

    fixed spacing issue

--- src/Mojo.class.php
+++ src/Mojo.class.php
@@ -10,68 +10,68 @@ ini_set('error_reporting','E_ALL');
 class Mojo
 {
 
-  public $config = array();
-  function __construct($arguments = array(), $options = array()) 
-  {
-    array_shift($arguments);
-    foreach($arguments as $k=>$v){
-      if(strpos($v,"=") > -1 || strpos($v,"-") > -1){
-          $v = str_replace("--","",$v);
-          $split = explode("=",$v);
-          $options[$split[0]] = $split[1];
-      }else{
+	public $config = array();
+	function __construct($arguments = array(), $options = array()) 
+	{
+		array_shift($arguments);
+		foreach($arguments as $k=>$v){
+			if(strpos($v,"=") > -1 || strpos($v,"-") > -1){
+				$v = str_replace("--","",$v);
+				$split = explode("=",$v);
+				$options[$split[0]] = $split[1];
+			}else{
 				unset($arguments[$k]);
-        switch($k):
-          case 0: $k="module";break;
-          case 1: $k="action";break;
-        endswitch;
-        $arguments[$k] = $v;
-      }
-    } 
+				switch($k):
+				case 0: $k="module";break;
+				case 1: $k="action";break;
+								endswitch;
+								$arguments[$k] = $v;
+			}
+		} 
 
-  	MojoConfig::bootstrap($arguments);
-    self::handler($arguments,$options);
-  }
+		MojoConfig::bootstrap($arguments);
+		self::handler($arguments,$options);
+	}
 
-  function handler($arguments = array(), $options = array())
-  {
+	function handler($arguments = array(), $options = array())
+	{
 
-    $class = (!empty($arguments['module']))?'Mojo'.$arguments['module']:false;
-    $action = (!empty($arguments['action']))?$arguments['action']:false;
+		$class = (!empty($arguments['module']))?'Mojo'.$arguments['module']:false;
+		$action = (!empty($arguments['action']))?$arguments['action']:false;
 
-    if(file_exists(MojoConfig::get('mojo_task_lib').$class.'.class.php')){ 
+		if(file_exists(MojoConfig::get('mojo_task_lib').$class.'.class.php')){ 
 
-      include_once(MojoConfig::get('mojo_task_lib').$class.'.class.php');
-      $$class = new $class($options);
+			include_once(MojoConfig::get('mojo_task_lib').$class.'.class.php');
+			$$class = new $class($options);
 
 
-      //if the module has a help method - run that, other wise, provide general
-      if(count($arguments) < 2 || array_key_exists("help",$options)){
-        if(method_exists($$class,"Help")) $$class->Help();
+			//if the module has a help method - run that, other wise, provide general
+			if(count($arguments) < 2 || array_key_exists("help",$options)){
+				if(method_exists($$class,"Help")) $$class->Help();
 				else MojoHelp::Docs();
-      }
+			}
 
-      if(method_exists($$class,$action)) $$class->$action();
-      else if(method_exists($$class,"Help")) $$class->Help();
+			if(method_exists($$class,$action)) $$class->$action();
+			else if(method_exists($$class,"Help")) $$class->Help();
 			else MojoHelp::Docs();
 
-    }else{
+		}else{
 
 			if(MojoConfig::get('mojo_task_lib')) MojoHelp::Docs();
 			else MojoConfig::Setup();
-    }
-  }
+		}
+	}
 
-  static function prompt($msg="")
-  {
-    echo "[mojo]: ".$msg."\n";
-  }
+	static function prompt($msg="")
+	{
+		echo "[mojo]: ".$msg."\n";
+	}
 
-  static function exception($msg="",$prefix=" - ERROR - ")
-  {
-    echo "\n[mojo]:".$prefix.' '.$msg."\n\n";
-    exit;
-  }
+	static function exception($msg="",$prefix=" - ERROR - ")
+	{
+		echo "\n[mojo]:".$prefix.' '.$msg."\n\n";
+		exit;
+	}
 
 	static function line($msg="\n")
 	{
@@ -80,4 +80,4 @@ class Mojo
 
 }
 
-  new Mojo($argv);
+new Mojo($argv);
