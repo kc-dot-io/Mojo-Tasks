8b5f016e14d23df428ed4289d93ee2e09225e70f
slajax <mail@slajax.com>
2010-07-12 14:30:05 -0400

    fixed input for windows systems atleast

--- src/MojoFunctions.php
+++ src/MojoFunctions.php
@@ -27,12 +27,31 @@ function getFile( $name = 'Mojo.class.php', $path = '.', $level = 0 )
   return $target;
 }
 
-function promptUser($question,$default)
+function promptUser($prompt,$default)
 {
-  echo "\n";
-  Mojo::prompt($question);
-  return trim(fgets(STDIN));
-
+  if (preg_match('/^win/i', PHP_OS)) {
+    $vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
+    file_put_contents(
+      $vbscript, 'wscript.echo(InputBox("'
+      . addslashes($prompt)
+      . '", "", "'.$default.'"))');
+    $command = "cscript //nologo " . escapeshellarg($vbscript);
+    $password = rtrim(shell_exec($command));
+    unlink($vbscript);
+    return $password;
+  } else {
+    $command = "/usr/bin/env bash -c 'echo OK'";
+    if (rtrim(shell_exec($command)) !== 'OK') {
+      trigger_error("Can't invoke bash");
+      return;
+    }
+    $command = "/usr/bin/env bash -c 'read -s -p \""
+      . addslashes($prompt)
+      . "\" mypassword && echo \$mypassword'";
+    $password = rtrim(shell_exec($command));
+    echo "\n";
+    return $password;
+  }
 }
 
 ?>
