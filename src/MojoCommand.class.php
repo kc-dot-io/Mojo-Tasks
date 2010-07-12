8dc20c27f65743662d4dc01b241165716ce0a7ee
slajax <mail@slajax.com>
2010-07-12 13:25:39 -0700

    changed Scaffolding to extend mojoFile for dynamic creation of sub directories and sub-components

--- src/MojoCommand.class.php
+++ src/MojoCommand.class.php
@@ -7,7 +7,7 @@
  * @author     Kyle Campbell
  */
 
-class MojoCommand extends Mojo
+class MojoCommand extends MojoFile
 {
   function __construct($args)
   {
@@ -25,16 +25,12 @@ class MojoCommand extends Mojo
       if(strpos($this->args['name'],'command.') < 1)
           return Mojo::prompt('The name you provided for your Command appears to be incorrect. '
                               .'Please use full Command path, ie: name=mojo.command.myCommand');
-      $source = self::Source();
-     
-      $name = explode('command.',$this->args['name']); $name = $name[1];
-      if(strpos($name,'.') > -1) { //handle commands in a sub dir
-          $tmp = explode('.',$name); //check if this dir exists, and create it if it does not
-          if(!file_exists(MojoConfig::get('sf_mojo_dir').'command/'.$tmp[0])) mkdir(MojoConfig::get('sf_mojo_dir').'command/'.$tmp[0]);
-          $name = $tmp[0].'/'.$tmp[1]; //full path including new dir
-      }
-      MojoFile::write(MojoConfig::get('mojo_js_dir').'command/'.$name.'.js',MojoFile::editStream($this->args,$source));
-      Mojo::prompt('Generated Command Scaffolding to '.MojoConfig::get('mojo_js_dir').'command/'.$name.'.js'); 
+                              
+      $source = self::Source();     
+      $file = self::makeNewFile($this->args['name'],'command');
+      
+      MojoFile::write($file,MojoFile::editStream($this->args,$source));
+      Mojo::prompt('Generated Command Scaffolding to '.$file); 
   }
 
   function Source()
